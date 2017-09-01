81ae719297aff7ce02827c980abf2d2c3bc12b5d
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-02 00:46:34 +0900

    test: add ParseActivity test

---
+++ activity.go
@@ -0,0 +1,40 @@
+package backlog
+
+import "time"
+
+const (
+	activitiesPath = "/api/v2/space/activities"
+)
+
+type Activity struct {
+	ID      int     `json:"id"`
+	Project Project `json:"project"`
+	Type    int     `json:"type"`
+	Content struct {
+		ID          int    `json:"id"`
+		KeyID       int    `json:"key_id"`
+		Summary     string `json:"summary"`
+		Description string `json:"description"`
+		Comment     struct {
+			ID      int    `json:"id"`
+			Content string `json:"content"`
+		}
+		Changes []struct {
+			Field    string `json:"field"`
+			NewValue string `json:"new_value"`
+			OldValue string `json:"old_value"`
+			Type     string `json:"type"`
+		} `json:"changes"`
+	} `json:"content"`
+
+	Notifications []struct {
+		ID                  int  `json:"id"`
+		AlreadyRead         bool `json:"alreadyRead"`
+		Reason              int  `json:"reason"`
+		User                User `json:"user"`
+		ResourceAlreadyRead bool `json:"resourceAlreadyRead"`
+	} `json:"notifications"`
+
+	CreatedUser User      `json:"createdUser"`
+	Created     time.Time `json:"created"`
+}
---
+++ activity_test.go
@@ -0,0 +1,99 @@
+package backlog
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestParseActivity(t *testing.T) {
+	rawJSON := []byte(`{
+	"id": 3153,
+	"project": {
+		"id": 92,
+		"projectKey": "SUB",
+		"name": "サブタスク",
+		"chartEnabled": true,
+		"subtaskingEnabled": true,
+		"projectLeaderCanEditProjectLeader": false,
+		"textFormattingRule": null,
+		"archived": false,
+		"displayOrder": 0
+	},
+	"type": 2,
+	"content": {
+		"id": 4809,
+		"key_id": 121,
+		"summary": "コメント",
+		"description": "test",
+		"comment": {
+			"id": 7237,
+			"content": "cont"
+		},
+		"changes": [
+			{
+				"field": "milestone",
+				"new_value": "R2014-07-23",
+				"old_value": "a",
+				"type": "standard"
+			},
+			{
+				"field": "status",
+				"new_value": "4",
+				"old_value": "1",
+				"type": "standard"
+			}
+		]
+	},
+	"notifications": [
+		{
+			"id": 25,
+			"alreadyRead": true,
+			"reason": 2,
+			"user": {
+				"id": 5686,
+				"userId": "takada",
+				"name": "takada",
+				"roleType": 2,
+				"lang": "ja",
+				"mailAddress": "takada@nulab.example"
+			},
+			"resourceAlreadyRead":true
+		}
+	],
+	"createdUser": {
+		"id": 1,
+		"userId": "admin",
+		"name": "admin",
+		"roleType": 1,
+		"lang": "ja",
+		"mailAddress": "eguchi@nulab.example"
+	},
+	"created": "2013-12-27T07:50:44Z"
+}`)
+	var activity Activity
+	if err := json.Unmarshal(rawJSON, &activity); err != nil {
+		t.Fatal(err)
+	}
+
+	assertInt(t, "ID", activity.ID, 3153)
+	assertInt(t, "Project#ID", activity.Project.ID, 92)
+	assertInt(t, "Type", activity.Type, 2)
+	assertInt(t, "Content#ID", activity.Content.ID, 4809)
+	assertInt(t, "Content#KeyID", activity.Content.KeyID, 121)
+	assertString(t, "Content#Summary", activity.Content.Summary, "コメント")
+	assertString(t, "Content#Description", activity.Content.Description, "test")
+	assertInt(t, "Content#Comment#ID", activity.Content.Comment.ID, 7237)
+	assertString(t, "Content#Comment#Content", activity.Content.Comment.Content, "cont")
+	assertString(t, "Content#Changes[0]#Field", activity.Content.Changes[0].Field, "milestone")
+	assertString(t, "Content#Changes[0]#NewValue", activity.Content.Changes[0].NewValue, "R2014-07-23")
+	assertString(t, "Content#Changes[0]#OldValue", activity.Content.Changes[0].OldValue, "a")
+	assertInt(t, "Notifications[0]#ID", activity.Notifications[0].ID, 25)
+	assertBool(t, "Notifications[0]#AlreadyRead", activity.Notifications[0].AlreadyRead, true)
+	assertInt(t, "Notifications[0]#Reason", activity.Notifications[0].Reason, 2)
+	assertInt(t, "Notifications[0]#User#ID", activity.Notifications[0].User.ID, 5686)
+	assertBool(t, "Notifications[0]#ResourceAlreadyRead", activity.Notifications[0].ResourceAlreadyRead, true)
+	assertInt(t, "CreatedUser", activity.CreatedUser.ID, 1)
+	assertInt(t, "Created#Year", activity.Created.Year(), 2013)
+	assertInt(t, "Created#Month", int(activity.Created.Month()), 12)
+	assertInt(t, "Created#Day", activity.Created.Day(), 27)
+}
