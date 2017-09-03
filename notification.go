a24be3ed49856e1240bcdd2cae240e1fa77db09d
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-03 23:12:27 +0900

    feat: add GetNotification

--- notification.go
+++ notification.go
@@ -1,8 +1,24 @@
 package backlog
 
-import "time"
+import (
+	"time"
+	"context"
+	"net/http"
+)
+
+const (
+	notificationPath = "/api/v2/space/notification"
+)
 
 type Notification struct {
 	Content string    `json:"content"`
 	Updated time.Time `json:"updated"`
+}
+
+func (c *Client) GetNotification(ctx context.Context) (*Notification, error) {
+	var out Notification
+	if err := c.get(ctx, notificationPath, http.StatusOK, &out); err != nil {
+		return nil, err
+	}
+	return &out, nil
 }
