99dfdf5e57e75a9bb725d64c9f2618302b7219f9
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-17 23:06:52 +0900

    feat: add GetCategories

--- category.go
+++ category.go
@@ -1,7 +1,26 @@
 package backlog
 
+import (
+	"context"
+	"net/http"
+	"path"
+)
+
+const (
+	categoriesPath = "categories"
+)
+
 type Category struct {
 	ID           int    `json:"id"`
 	Name         string `json:"name"`
 	DisplayOrder int    `json:"displayOrder"`
+}
+
+func (c *Client) GetCategories(ctx context.Context, projectID string) ([]Category, error) {
+	spath := path.Join(projectsPath, projectID, categoriesPath)
+	var categories []Category
+	if err := c.get(ctx, spath, http.StatusOK, &categories); err != nil {
+		return nil, err
+	}
+	return categories, nil
 }
